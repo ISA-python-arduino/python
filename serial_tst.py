cc627bcf93673fab95128803b48e78cdc80dac69
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2018-10-02 15:37:45 +0200

    a

--- serial_tst.py
+++ serial_tst.py
@@ -7,6 +7,7 @@ print('Serial script started');
 ser = serial.Serial('/dev/ttyACM0', 9600);
 
 while True:
+    ser.writeline('echo');
     out = ser.readline().decode("utf-8")
-    num = re.findall(r"[-+]?\d*\.\d+|\d+", out);
-    print(num[0]);
+    #num = re.findall(r"[-+]?\d*\.\d+|\d+", out);
+    print('Arduino ' + out);
