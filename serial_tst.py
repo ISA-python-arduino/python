8c2c7faab24567663acb165de4754b8a22222ec0
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2018-10-02 15:25:00 +0200

    replace

--- serial_tst.py
+++ serial_tst.py
@@ -6,7 +6,9 @@ print('Serial script started');
 ser = serial.Serial('/dev/ttyACM0', 9600);
 
 while True:
-    out = ser.readline().decode("utf-8") 
+    out = ser.readline()
+    print(out)
+    out = out.decode("utf-8")
     out = out.replace('X=', '').replace('out=', '').replace('wart_star=', '').replace(',','');
     print(out);
     time.sleep(1);
