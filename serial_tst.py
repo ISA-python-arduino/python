a8c821a12ac88fbdf1259642e521864efd16b71f
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2018-10-02 15:28:58 +0200

    replace

--- serial_tst.py
+++ serial_tst.py
@@ -1,13 +1,12 @@
 import serial
 import time
+import re
 
 print('Serial script started');
 
 ser = serial.Serial('/dev/ttyACM0', 9600);
 
 while True:
-    out = ser.readline()
-    print(out)
     out = out.decode("utf-8")
-    out = out.replace('X=', '').replace('out=', '').replace('wart_star=', '').replace(',','');
-    print(out);
+    num = re.findAll(r"[-+]?\d*\.\d+|\d+", out);
+    print(num);
