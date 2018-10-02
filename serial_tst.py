11b3fd0c7eca300b1c1aafe235c676dfca9376cc
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2018-10-02 15:30:15 +0200

    replace

--- serial_tst.py
+++ serial_tst.py
@@ -8,5 +8,5 @@ ser = serial.Serial('/dev/ttyACM0', 9600);
 
 while True:
     out = ser.readline().decode("utf-8")
-    num = re.findAll(r"[-+]?\d*\.\d+|\d+", out);
+    num = re.findall(r"[-+]?\d*\.\d+|\d+", out);
     print(num);
