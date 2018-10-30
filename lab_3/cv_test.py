064320723a6eeb7c763efd442339299ab237c338
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2018-10-30 14:55:27 +0100

    yaw

--- lab_3/cv_test.py
+++ lab_3/cv_test.py
@@ -1,5 +1,5 @@
 import time
-#import serial
+import serial
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import numpy as np
@@ -37,7 +37,7 @@ roiSize = (6, 6) # roi size on the scaled down image (converted to HSV)
 
 
 # initialize serial communication
-#ser = serial.Serial(port='/dev/ttyACM0', baudrate=57600, timeout=0.05)
+ser = serial.Serial(port='/dev/ttyACM0', baudrate=57600, timeout=0.05)
 
 # while True:
 for cameraFrame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -110,11 +110,11 @@ for cameraFrame in camera.capture_continuous(rawCapture, format="bgr", use_video
             scaled = (translate(distanceVector[0], -width//2, width//2), translate(distanceVector[1], -height//2, height//2) )
             # print("Vector scaled: {}".format(scaled))
             cv2.line(upscaledColor, screenMiddle, biggestObjectMiddle, (0, 0, 255))
-            #yaw = 'y {}\n'.format(scaled[0])
-            #b_yaw = bytes(yaw, 'utf-8') # or 'ascii'
+            yaw = 'y {}\n'.format(scaled[0])
+            b_yaw = bytes(yaw, 'utf-8') # or 'ascii'
             #pitch = 'p {}\n'.format(scaled[1])
             #b_pitch = bytes(pitch, 'utf-8') # or 'ascii'
-            #ser.write(b_yaw)
+            ser.write(b_yaw)
             # print(ser.read_all())
             #ser.write(b_pitch)
             # print(ser.read_all())
