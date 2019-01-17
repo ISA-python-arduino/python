a817301007261dae278bf714b01bab307688911d
Bartosz Ujazdowski <b.ujazdowski@gmail.com>
2019-01-17 22:17:10 +0100

    Wybor funkcji do wykrywania

--- lab5/main.py
+++ lab5/main.py
@@ -5,17 +5,25 @@ import cv2
 import random as rng
 import math
 import serial
+
 ARDUINO_MODE = False
 DEBUG = True
 USE_PI_CAMERA = ARDUINO_MODE
 CAMERA_RESOLUTION = (640, 480)
-PERCENT_IN_TORUS_TO_ACCEPT = 0.6    # Określa jaki procent punktów musi się znajdować w torusie aby uznać kształt za okrąg
-SEND_STOP_TIME = 1.0                # Czas po jakim wysyłana jest komenda stopu do Arduino
+PERCENT_IN_TORUS_TO_ACCEPT = 0.6    # Określa jaki procent punktów musi się znajdować w torusie aby uznać kształt za okrąg.
+SEND_STOP_TIME = 1.0                # Czas po jakim wysyłana jest komenda stopu do Arduino. Jest to czas zużycia procesora.
 SCALE_FACTOR = 4
 KERNEL = (9, 9)
-MINIMUM_POINTS_IN_CONTOUR = 15      # Minimalna ilość punktów w konturze potrzebnych do podjęcia próby wykrywania okręgów
-MINIMUM_MOMENT = 0.05               # Minimalna wyliczona wartość ze wzoru aby uznać kontur za okrąg
-INNER_RADIUS_MULTIPLIER = 0.9       # Promień wewnętrznego okrąg w toruse
+MINIMUM_POINTS_IN_CONTOUR = 15      # Minimalna ilość punktów w konturze potrzebnych do podjęcia próby wykrywania okręgów.
+MINIMUM_MOMENT = 0.15               # Minimalna wyliczona wartość ze wzoru aby uznać kontur za okrąg.
+INNER_RADIUS_MULTIPLIER = 0.9       # Promień wewnętrznego okręgu w toruse.
+ROI_SIZE = (6, 6)
+
+FIND_CONTOUR_METHOD = 0             # 0 - połączone, 1 - momenty, 2 - torus
+
+BGR_RED = (0,0,255)
+BGR_BLUE = (255,0,0)
+BGR_ORANGE = (0,150,255)
 
 stopMessage = time.process_time()
 
@@ -26,8 +34,6 @@ UPPER_COLOR = (33, 255, 255)
 if not ARDUINO_MODE:
     LOWER_COLOR = (40, 100, 50)
     UPPER_COLOR = (80, 255, 255)
-
-roiSize = (6, 6)
 
 if ARDUINO_MODE:
     ser = serial.Serial(port='/dev/ttyACM0', baudrate=57600, timeout=0.05)
@@ -56,8 +62,8 @@ def checkIfCircleMomentsMethod(contour):
         tempMoment = (maxMoment / minMoment) - 1.0
         tempMoment = tempMoment * tempMoment
         tempMoment = tempMoment / (1.0 - M['nu11']) * (1.0 - M['nu11'])
+        debug('Moment: {}'.format(tempMoment))
         if  tempMoment > 0.0 and tempMoment < MINIMUM_MOMENT:
-            debug('Moment: {}'.format(tempMoment))
             posX = m10 / momentArea
             posY = m01 / momentArea
             return (posX > 0 and posY > 0)
@@ -103,13 +109,21 @@ def stopCommand(stopMessage):
     return stopMessage
 
 def roi(image):
-    return image[newHeight//2 - roiSize[0]//2 : newHeight //2 + roiSize[0]//2, newWidth//2 - roiSize[1]//2 : newWidth//2 + roiSize[1]//2, :]
+    return image[newHeight//2 - ROI_SIZE[0]//2 : newHeight //2 + ROI_SIZE[0]//2, newWidth//2 - ROI_SIZE[1]//2 : newWidth//2 + ROI_SIZE[1]//2, :]
 
 def quit(): 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         return True
     return False
+
+def findCircle(mode, contour):
+    if mode is 0:
+        return checkIfCircleMomentsMethod(contour) and checkMinimumPointsInTorus(contour)
+    elif mode is 1:
+        return checkIfCircleMomentsMethod(contour)
+    else :
+        return checkMinimumPointsInTorus(contour)
 
 time.sleep(2.0)
 
@@ -133,7 +147,7 @@ while not quit():
     if contours:            
         for i, contour in enumerate(contours):
             if len(contour) >= MINIMUM_POINTS_IN_CONTOUR:
-                if checkIfCircleMomentsMethod(contour) and checkMinimumPointsInTorus(contour):
+                if findCircle(FIND_CONTOUR_METHOD, contour):
                     filteredContours.append(contour)
     else:
         pass
@@ -148,9 +162,9 @@ while not quit():
             cv2.drawContours(upscaledColor, [largestContour * [SCALE_FACTOR, SCALE_FACTOR]], -1, (255,0,0), thickness=3)
             (x,y),radius = cv2.minEnclosingCircle(largestContour * [SCALE_FACTOR, SCALE_FACTOR])
             center = (int(x),int(y))
+            cv2.circle(upscaledColor, center, int(radius), BGR_ORANGE, thickness=1)
             radius = int(radius*INNER_RADIUS_MULTIPLIER)
-            cv2.circle(upscaledColor, center, radius, (0,255,255), thickness=1)
-            cv2.circle(upscaledColor, center, radius, (0,0,255), thickness=1)
+            cv2.circle(upscaledColor, center, radius, BGR_RED, thickness=1)
                 
     if biggestObject_BoundingBox:
         x, y, w, h = biggestObject_BoundingBox
@@ -158,13 +172,13 @@ while not quit():
         screenMiddle = width//2, height//2
         distanceVector = tuple(map(lambda x, y: x - y, biggestObjectMiddle, screenMiddle))
         scaled = (translate(distanceVector[0], -width//2, width//2), translate(distanceVector[1], -height//2, height//2) )
-        cv2.line(upscaledColor, screenMiddle, biggestObjectMiddle, (0, 0, 255))
+        cv2.line(upscaledColor, screenMiddle, biggestObjectMiddle, BGR_RED)
 
         sendPacket('packet', scaled[0], scaled[1])
         stopMessage = time.process_time()
         if not ARDUINO_MODE:
-            cv2.circle(upscaledColor, biggestObjectMiddle, 2, (255, 0, 0), thickness=2)
-            cv2.rectangle(resizedColor, (x, y), (x+w, y+h), (0, 0, 255), thickness=2)
+            cv2.circle(upscaledColor, biggestObjectMiddle, 2, BGR_BLUE, thickness=2)
+            cv2.rectangle(resizedColor, (x, y), (x+w, y+h), BGR_RED, thickness=2)
     else:
         stopMessage = stopCommand(stopMessage)
                 
@@ -172,6 +186,8 @@ while not quit():
         cv2.imshow("video", upscaledColor)
         cv2.imshow("roi", roi(resizedHSV))
         cv2.imshow("mask", mask)
-			
+
+    debug('--------------------------------')
+
 cv2.destroyAllWindows()
 vs.stop()
